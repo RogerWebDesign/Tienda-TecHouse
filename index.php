94fa069fbba3aa9ac4b55d75c10697cc0085ee58
Roger <roger@roger.com>
2020-02-28 14:51:52 -0500

    Inicio de la maquetacion de la tienda online

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Tienda - TecHouse</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="assets/img/Logo-TecHouse.png">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/fuente.css">
+    </head>
+    <body>
+        <header>
+            <div id="logo">
+                <a href="">
+                    <img src="assets/img/Logo-TecHouse.png" alt="Logo-TecHouse">
+                </a>
+                <h1>Hola mundo con fuente proxima nova</h1>
+            </div>
+        </header>
+    </body>
+</html>
